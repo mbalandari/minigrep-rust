3a875351b9563a3adfbd272527db0a8b0f894a89
codingMaz <m.balandari.toroghi@gmail.com>
2025-10-21 12:24:21 +0330

    add fisrt test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
